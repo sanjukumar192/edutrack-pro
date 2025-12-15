4d18648f214729108ed56f2ac2c9b9f2ef424d56
sanjukumar192 <sanjukumar192@gmail.com>
2025-12-15 20:55:19 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,16 +3,17 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2EsjDjbZG2Ycz3R4y2CYi0mBGHUIGNY",
+  apiKey: "AIzaSyC2EsjDjibZG2Ycz3R4y2CYiOmBGHUIGNY",
   authDomain: "edutrack-pro-c00f7.firebaseapp.com",
   projectId: "edutrack-pro-c00f7",
-  storageBucket: "edutrack-pro-c00f7.appspot.com",
+  storageBucket: "edutrack-pro-c00f7.firebasestorage.app",
   messagingSenderId: "997794547268",
   appId: "1:997794547268:web:48beb47705d249b7c7de11",
-  measurementId: "G-1H93V31GN"
+  measurementId: "G-1H93V310GN"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
 export const auth = getAuth(app);
